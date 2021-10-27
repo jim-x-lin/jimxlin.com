16e1078f58204538ad2a3a2528e8591ba08f9e07
Jim X. Lin <miljinx@gmail.com>
2021-10-27 00:16:58 -0700

    Add dark mode

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 function decode(string) {
   const key = parseInt(string.substr(0, 2), 36);
   var output = "";
-  for (let i = 2; i < string.length; i += 2) {
+  for (var i = 2; i < string.length; i += 2) {
     output += String.fromCharCode(key ^ parseInt(string.substr(i, 2), 36));
   }
   return output;
@@ -15,7 +15,7 @@ function decode(string) {
 function encode(string) {
   const key = Math.ceil(Math.random() * 1295);
   var output = key.toString(36);
-  for (let i = 0; i < string.length; i += 1) {
+  for (var i = 0; i < string.length; i += 1) {
     const encoded = (key ^ string.charCodeAt(i)).toString(36);
     output += encoded.length === 1 ? '0' + encoded : encoded;
   }
@@ -24,12 +24,68 @@ function encode(string) {
 
 function decodeContacts() {
   var links = document.querySelectorAll("#contact a");
-  links.forEach(function(link) {
+  for (var i = 0; i < links.length; i += 1) {
+    var link = links[i];
     link.setAttribute("href", decode(link.getAttribute("href").trim()));
     link.innerHTML = decode(link.innerHTML.trim());
-  });
+  }
+}
+
+/*************
+ * Dark Mode *
+ *************/
+
+function makeDark() {
+  var bodyEl = document.querySelector("body");
+  bodyEl.style.color = "#fff";
+  bodyEl.style.background = "#000";
+  var headerEl = document.querySelector("header");
+  headerEl.style.color = "#fff";
+  headerEl.style.background = "#000";
+  var toggleEl = document.querySelector("#toggleDark");
+  toggleEl.innerHTML = "Light";
+  toggleEl.style.color = "#000";
+  toggleEl.style.background = "#fff";
+  styleMultiple("a", "color", "#fff");
+}
+
+function makeLight() {
+  var bodyEl = document.querySelector("body");
+  bodyEl.style.color = "#000";
+  bodyEl.style.background = "#fff";
+  var headerEl = document.querySelector("header");
+  headerEl.style.color = "#000";
+  headerEl.style.background = "#fff";
+  var toggleEl = document.querySelector("#toggleDark");
+  toggleEl.innerHTML = "Dark";
+  toggleEl.style.color = "#fff";
+  toggleEl.style.background = "#000";
+  styleMultiple("a", "color", "#000");
+}
+
+function toggleDark() {
+  if (localStorage.getItem("darkmode")) {
+    makeLight();
+    localStorage.removeItem("darkmode");
+  } else {
+    makeDark();
+    localStorage.setItem("darkmode", "true");
+  }
+}
+
+/***********
+ * Helpers *
+ ***********/
+
+function styleMultiple(selector, property, value) {
+  var els = document.querySelectorAll(selector)
+  for (var i = 0; i < els.length; i += 1) {
+    els[i].style[property] = value;
+  }
 }
 
 window.onload = function() {
   decodeContacts();
+  if (localStorage.getItem("darkmode")) makeDark();
+  document.querySelector("#toggleDark").onclick = toggleDark;
 }
